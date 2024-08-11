cd9c1e1b1cce3c52ad2e0c1432d08c6ca9b4a189
CH-Chang <ch.chang.software@gmail.com>
2024-08-11 21:49:32 +0800

    feat: add log cleanup

--- src/crons/utils/logger.ts
+++ src/crons/utils/logger.ts
@@ -3,6 +3,10 @@ import dayjs from 'dayjs'
 
 export const log = async (env: Env, status: boolean, id: number, memo: string): Promise<void> => {
 	await env.DB
+		.prepare("DELETE FROM TB_PUNCH_IN_LOG WHERE punch_in_datetime < DATETIME('now', '-7 day', 'localtime')")
+		.run()
+
+	await env.DB
 		.prepare('INSERT INTO TB_PUNCH_IN_LOG (punch_in_id, punch_in_datetime, punch_in_status, punch_in_memo) VALUES (?1, ?2, ?3, ?4)')
 		.bind(
 			id,
