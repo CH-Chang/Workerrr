0e686dc16ded95b362d17d8324bdfbd66f55663f
CH-Chang <ch.chang.software@gmail.com>
2024-08-25 20:40:47 +0800

    feat: update sql

--- worker/src/crons/crons/punch/index.ts
+++ worker/src/crons/crons/punch/index.ts
@@ -11,12 +11,12 @@ interface PunchCronTaskArguments {
 const queryPunchIn = async (env: Env, punchInBatch: number): Promise<{ success: boolean, punchIns: PunchInRow[] }> => {
 	const stat = env.DB
 		.prepare(`
-			SELECT P.punch_in_id        AS punchInId
-				,P.user_id           AS userId
-				,P.punch_in_type     AS punchInType
-				,P.punch_in_account  AS punchInAccount
-				,P.punch_in_password AS punchInPassword
-				,P.notify_email      AS notifyEmail
+			SELECT P.punch_in_id       AS punchInId,
+				P.user_id           AS userId,
+				P.punch_in_type     AS punchInType,
+				P.punch_in_account  AS punchInAccount,
+				P.punch_in_password AS punchInPassword,
+				P.notify_email      AS notifyEmail
 			FROM   TB_PUNCH_IN AS P
 			WHERE  P.punch_in_enable = 'Y'
 				AND P.punch_in_batch = ?1
@@ -24,7 +24,14 @@ const queryPunchIn = async (env: Env, punchInBatch: number): Promise<{ success: 
 											FROM   TB_PUNCH_IN_LOG AS L
 											WHERE  L.punch_in_status = 'Success'
 													AND L.punch_in_id = P.punch_in_id
-													AND SUBSTR(L.punch_in_datetime, 0, 11) = DATE('now', 'localtime')); `)
+													AND Substr(L.punch_in_datetime, 0, 11) =
+														Date('now', 'localtime'))
+				AND P.punch_in_id NOT IN (SELECT M.punch_in_id
+											FROM   TB_PUNCH_IN_MANUAL M
+											WHERE  M.punch_in_manual_type = 'Cancel'
+													AND M.punch_in_manual_date =
+														Date('now', 'localtime')
+													AND M.punch_in_id = P.punch_in_id);  `)
 		.bind(
 			punchInBatch
 		)
