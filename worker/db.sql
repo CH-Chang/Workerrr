8731d4d6e9d0df4c6eed4858c91578a7135c5b49
CH-Chang <ch.chang.software@gmail.com>
2024-09-05 12:56:12 +0800

    feat: add otp table

--- worker/db.sql
+++ worker/db.sql
@@ -5,3 +5,4 @@ CREATE TABLE IF NOT EXISTS TB_USER (user_id INTEGER PRIMARY KEY AUTOINCREMENT, a
 CREATE TABLE IF NOT EXISTS TB_CRON (cron_id INTEGER PRIMARY KEY AUTOINCREMENT, cron_key TEXT, cron_task TEXT, cron_arguments TEXT);
 CREATE TABLE IF NOT EXISTS TB_PUNCH_IN_MANUAL (punch_in_manual_id INTEGER PRIMARY KEY AUTOINCREMENT, punch_in_id INTEGER, punch_in_manual_date TEXT, punch_in_manual_type TEXT, punch_in_manual_argument TEXT);
 CREATE TABLE IF NOT EXISTS TB_SCHEDULE (schedule_id INTEGER PRIMARY KEY AUTOINCREMENT, schedule_token TEXT, punch_in_id INTEGER, schedule_expiration_datetime TEXT);
+CREATE TABLE IF NOT EXISTS TB_OTP (otp_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id TEXT, otp_key TEXT, otp TEXT, otp_type TEXT, otp_purpose TEXT, otp_argument TEXT, otp_expiration_datetime TEXT);
