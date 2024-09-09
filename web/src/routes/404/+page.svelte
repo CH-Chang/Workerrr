8081aee74201493874ae867d395772997e0fce64
CH-Chang <ch.chang.software@gmail.com>
2024-09-09 12:51:15 +0800

    feat: adjust 404 page style

--- web/src/routes/404/+page.svelte
+++ web/src/routes/404/+page.svelte
@@ -1,14 +1,14 @@
 <div class="w-screen h-screen bg-stone-800">
 	<div class="fixed top-20 left-8">
-		<p class="text-xl font-noto font-black text-zinc-200">404<br /> NOT FOUND</p>
+		<p class="md:text-xl text-md font-noto font-black text-zinc-200">404<br /> NOT FOUND</p>
 	</div>
 	<div class="fixed bottom-20 right-8">
-		<p class="text-xl font-noto font-black text-zinc-200 text-right">404<br /> NOT FOUND</p>
+		<p class="md:text-xl text-md font-noto font-black text-zinc-200 text-right">404<br /> NOT FOUND</p>
 	</div>
 	<div class="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
 		<div>
-			<h1 class="text-4xl font-noto font-black -rotate-12 text-zinc-200">Opps!</h1>
-			<h2 class="text-2xl font-noto font-bold ml-14 mt-6 text-zinc-200">打工人失蹤了！</h2>
+			<h1 class="md:text-4xl text-2xl font-noto font-black -rotate-12 text-zinc-200">Opps!</h1>
+			<h2 class="md:text-2xl text-md font-noto font-bold ml-14 mt-6 text-zinc-200">打工人失蹤了！</h2>
 		</div>
 		<div class="flex flex-row items-center justify-center mt-14 gap-4">
 			<a href="/">
@@ -19,7 +19,8 @@
 			</a>
 		</div>
 	</div>
-	<div class="fixed bottom-5 left-[50%] translate-x-[-50%] font-noto">
-		<p class="text-md text-zinc-200">Copyright © 2024 Workerrr. All rights reserved.</p>
+	<div class="fixed bottom-5 left-[50%] translate-x-[-50%] flex flex-col items-center justify-center">
+		<p class="md:text-md text-sm text-zinc-200 font-noto">Copyright © 2024 Workerrr</p>
+		<p class="md:text-md text-sm text-zinc-200 font-noto">All rights reserved</p>
 	</div>
 </div>
