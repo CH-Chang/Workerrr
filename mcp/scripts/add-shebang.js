f2fbc88b71b080f41df154b03eae5f44ff7dec07
CH-Chang <ch.chang.software@gmail.com>
2025-04-25 15:30:59 +0800

    fix: solve shebang issue

---
+++ mcp/scripts/add-shebang.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '../dist/index.js');
+const shebang = '#!/usr/bin/env node\n';
+
+const content = fs.readFileSync(filePath, 'utf8');
+if (!content.startsWith(shebang)) {
+  fs.writeFileSync(filePath, shebang + content);
+}
